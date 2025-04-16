e918a729a8ee4c9aaaf7a286f3ef08d5247810e6
Artem Kabritskii <artemkabr7@gmail.com>
2025-04-16 12:52:10 +0300

    chore: masks.py project structure and add base files from previous work

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.masks import get_mask_card_number, get_mask_account
+# from src.masks import get_mask_card_number
+
+print(get_mask_card_number("7000792289606361"))
+
+
+# from src.masks import get_mask_account
+
+print(get_mask_account("7000792289606361"))
