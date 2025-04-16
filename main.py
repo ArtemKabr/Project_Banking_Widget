61860ff952a852647555f538b84f04dda8ac312d
Artem Kabritskii <artemkabr7@gmail.com>
2025-04-16 13:31:45 +0300

    Add function that can process information about both cards and accounts

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from src.masks import get_mask_card_number, get_mask_account
-# from src.masks import get_mask_card_number
+from src.widget import get_mask_card_number, get_mask_account, mask_account_card
+# from src.widget import get_mask_card_number
 
 print(get_mask_card_number("7000792289606361"))
 
 
-# from src.masks import get_mask_account
+# from src.widget import get_mask_account
 
-print(get_mask_account("7000792289606361"))
+print(get_mask_account("700079228960636"))
+
+
+# from src.widget import mask_account_card
+
+print(mask_account_card("Maestro 1596837868705199"),'\n',
+      mask_account_card('Счет 64686473678894779589'),'\n',
+      mask_account_card('MasterCard 7158300734726758'),'\n',
+      mask_account_card('Счет 35383033474447895560'),'\n',
+      mask_account_card('Visa Classic 6831982476737658'),'\n',
+      mask_account_card('Visa Platinum 8990922113665229'),'\n',
+      mask_account_card('Visa Gold 5999414228426353'),'\n',
+      mask_account_card('Счет 73654108430135874305'),
+)
+
---
+++ src/widget.py
@@ -0,0 +1,72 @@
+import re
+
+def get_mask_card_number(card_number: str) -> str:
+    """ Маскирует номер банковской карты,
+    отображая только первые 6 цифр и последние 4 цифры.
+
+    Параметры:
+    card_number (str): Номер банковской карты (должен быть строкой из 16 цифр).
+
+    Возвращает:
+    str: Маскированный номер карты в формате 'XXXX XX** **** XXXX'.
+
+    Исключения:
+    ValueError: Если номер карты не состоит из 16 цифр."""
+    # Проверяем, что номер карты состоит из 16 цифр
+    if len(card_number) != 16 or not card_number.isdigit():
+        raise ValueError("Номер карты должен содержать 16 цифр.")
+
+    # Формируем маску
+    masked_number = (
+        f"{card_number[:4]} {card_number[4:6]}**" f" **** {card_number[-4:]}"
+    )
+
+    return masked_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """ Маскирует номер банковского счета, отображая только последние 4 цифры.
+
+    Параметры:
+    account_number (str): Номер банковского счета
+    (должен быть строкой из цифр).
+
+    Возвращает:
+    str: Маскированный номер счета в формате '**XXXX'.
+
+    Исключения:
+    ValueError: Если номер счета содержит не только цифры."""
+    # Проверяем, что номер счёта состоит только из цифр
+    if not account_number.isdigit():
+        raise ValueError("Номер счета должен содержать только цифры.")
+
+    # Маскируем номер счёта, оставляя только последние 4 цифры
+    masked_account = f"**{account_number[-4:]}"
+
+    return masked_account
+
+
+def mask_account_card(account_info: str) -> str:
+    """Обрабатывает информацию о банковской карте или счете и возвращает маскированный номер.
+
+    Параметры:
+    account_info (str): Строка, содержащая тип и номер карты или счета.
+
+    Возвращает:
+    str: Строка с замаскированным номером.
+    """
+    # Разбиваем строку на тип и номер
+    match = re.match(r"(\D+)\s(\d+)", account_info.strip())
+    if match:
+        account_type = match.group(1).strip()
+        account_number = match.group(2).strip()
+
+        if "Счет" in account_type:
+            # Если тип "Счет", маскируем номер счета
+            return f"{account_type} {get_mask_account(account_number)}"
+        else:
+            # Если это карта, маскируем номер карты
+            return f"{account_type} {get_mask_card_number(account_number)}"
+    else:
+        return "Неверный формат ввода."
+
