f981fc311ce45a5e178bcc6b68b3759a6f398cef
Artem Kabritskii <artemkabr7@gmail.com>
2025-04-25 10:59:45 +0300

    added the processing module, it created two new functions filter_by_state, sort_by_date, checked for isort, flake8, black, made additions and adjustments to the README file.

---
+++ src/processing.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Any, Dict, List
+
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+
+    Параметры:
+        data (List[Dict[str, Any]]): Список операций.
+        state (str): Статус операции, по умолчанию 'EXECUTED'.
+
+    Возвращает:
+        List[Dict[str, Any]]: Отфильтрованный список операций.
+    """
+    return [item for item in data if item.get("state") == state]
+
+
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
+    """
+    Сортирует список словарей по ключу 'date'.
+
+    :param data: Список операций, содержащих ключ 'date'
+    :param descending: Порядок сортировки. По убыванию — True (по умолчанию),
+                       по возрастанию — False
+    :return: Новый список, отсортированный по дате
+    """
+    return sorted(
+        data,
+        key=lambda x: datetime.fromisoformat(x["date"]),
+        reverse=descending,
+    )
